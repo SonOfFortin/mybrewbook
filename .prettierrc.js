fb06906fa0e526dc9fa7c8286a974687c957007f
Raphael Fortin <cannibal_c@msn.com>
2023-06-20 04:50:24 -0400

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    trailingComma: 'none',
+    useTabs: false,
+    arrowParens: 'avoid'
+  };
